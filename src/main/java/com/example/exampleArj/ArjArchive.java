7eda7fb0fe344ffa496af3c37b5e98895c74032b
niyazkadirov <niyaz.kadirov@yandex.ru>
2020-02-07 22:42:54 +0300

    refactoring

--- src/main/java/com/example/exampleArj/ArjArchive.java
+++ src/main/java/com/example/exampleArj/ArjArchive.java
@@ -1,44 +1,32 @@
 package com.example.exampleArj;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveException;
-import org.apache.commons.compress.archivers.ArchiveInputStream;
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
-import org.apache.commons.compress.archivers.arj.ArjArchiveEntry;
 import org.apache.commons.compress.archivers.arj.ArjArchiveInputStream;
 import org.apache.commons.io.IOUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 
 public class ArjArchive {
 
     public static void main(String[] args) throws IOException, ArchiveException {
-        new ArjArchive().unzip("c:/5.arj", "C:/Users/1/Desktop/files");
-    }
-
-    private static String getDir(ArjArchiveEntry entry) {
-        File file = new File(entry.getName());
-        return entry.getName().replaceAll(file.getName(), "");
+        unzip("bla.arj", "c:/example/");
     }
 
 
-    public void unzip(String src, String dir) throws IOException, ArchiveException {
-
-        InputStream is = new FileInputStream(new File(src));
-
-
-
-        try (ArjArchiveInputStream i = new ArjArchiveInputStream(new FileInputStream(new File(src)))) {
-            ArchiveEntry entry = null;
-
+    private static void unzip(String in, String out) throws IOException, ArchiveException {
+        try (ArjArchiveInputStream i = new ArjArchiveInputStream(new FileInputStream(new File(in)))) {
+            ArchiveEntry entry;
             while ((entry = i.getNextEntry()) != null) {
                 if (!i.canReadEntryData(entry)) {
-                    System.out.println("Error: can't read" + entry.getName());
+                    System.out.println("Error: can't read " + entry.getName());
                     continue;
                 }
-                String name = dir + entry.getName();
+                String name = out + entry.getName();
                 File f = new File(name);
                 if (entry.isDirectory()) {
                     if (!f.isDirectory() && !f.mkdirs()) {
@@ -50,12 +38,10 @@ public class ArjArchive {
                         throw new IOException("failed to create directory " + parent);
                     }
                     try (OutputStream o = Files.newOutputStream(f.toPath())) {
-                       IOUtils.copy(i, o);
-
-                        }
+                        IOUtils.copy(i, o);
                     }
                 }
             }
         }
     }
-
+}
